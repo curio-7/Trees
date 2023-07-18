7ad1654a348cf27c3f9f9a28460e3621bfb7fe6e
Sarthak Nigam <77585810+curio-7@users.noreply.github.com>
2023-07-18 19:45:18 +0530

    Time: 44 ms (23.31%), Space: 30.7 MB (99.10%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int lh(TreeNode* root){
+        int h = 0;
+        while(root){
+            root=root->left;
+            h++;
+        }
+        return h;
+    }
+    int rh(TreeNode* root){
+        int h = 0;
+        while(root){
+            root=root->right;
+            h++;
+        }
+        return h;
+    }
+    int countNodes(TreeNode* root) {
+//         if(root == NULL)
+//         return 0;
+    
+//         int res = 0;
+//         queue<TreeNode *> q;
+//         q.push(root);
+//         while(!q.empty())
+//         {
+
+//             int n = q.size();
+//             while(n--){
+//                 res++;
+//                 auto node = q.front();q.pop();
+//                 if(node->left)q.push(node->left);
+//                 if(node->right)q.push(node->right);
+//             }
+//         }
+
+//         return res;
+        //O(N)
+        //now another
+        if(!root)return 0;
+        int lefth = lh(root);
+        int righth = rh(root);
+        if(lefth==righth) return (1<<lefth)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
+        //TC = O(logn)^2
+        //SC = O(logn)
+    }
+};
